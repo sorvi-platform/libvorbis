b57e8e88de8d1c83134db1ddbd1f19a448f52a91
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2026-01-20 13:05:41 +0900

    update to zig 0.15.2, and combine libogg/libvorbis

--- build.zig
+++ build.zig
@@ -4,50 +4,66 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const libogg_dep = b.dependency("libogg", .{
-        .target = target,
-        .optimize = optimize,
+    const libogg = b.addLibrary(.{
+        .name = "ogg",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+            .sanitize_c = .off,
+        }),
+        .linkage = .static,
     });
+    libogg.root_module.addIncludePath(b.path("include"));
+    libogg.root_module.addCSourceFiles(.{
+        .flags = &.{"-std=c99", "-fvisibility=hidden"},
+        .files = &.{ "bitwise.c", "framing.c" },
+        .root = b.path("libogg"),
+    });
+    libogg.installHeadersDirectory(b.path("include/ogg"), "ogg", .{});
+    b.installArtifact(libogg);
 
-    const lib = b.addLibrary(.{
+    const libvorbis = b.addLibrary(.{
         .name = "vorbis",
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
+            .sanitize_c = .off,
         }),
-        .linkage = std.builtin.LinkMode.static,
+        .linkage = .static,
     });
-    lib.linkLibrary(libogg_dep.artifact("ogg"));
-    lib.addIncludePath(b.path("include"));
-    lib.addIncludePath(b.path("lib"));
-    lib.addCSourceFiles(.{
+    libvorbis.root_module.linkLibrary(libogg);
+    libvorbis.root_module.addIncludePath(b.path("include"));
+    libvorbis.root_module.addIncludePath(b.path("libvorbis"));
+    libvorbis.root_module.addCSourceFiles(.{
+        .flags = &.{"-fvisibility=hidden"},
         .files = &.{
-            "lib/analysis.c",
-            "lib/bitrate.c",
-            "lib/block.c",
-            "lib/codebook.c",
-            "lib/envelope.c",
-            "lib/floor0.c",
-            "lib/floor1.c",
-            "lib/info.c",
-            "lib/lookup.c",
-            "lib/lpc.c",
-            "lib/lsp.c",
-            "lib/mapping0.c",
-            "lib/mdct.c",
-            "lib/psy.c",
-            "lib/registry.c",
-            "lib/res0.c",
-            "lib/sharedbook.c",
-            "lib/smallft.c",
-            "lib/synthesis.c",
-            "lib/window.c",
-
-            "lib/vorbisfile.c",
-            "lib/vorbisenc.c",
+            "analysis.c",
+            "bitrate.c",
+            "block.c",
+            "codebook.c",
+            "envelope.c",
+            "floor0.c",
+            "floor1.c",
+            "info.c",
+            "lookup.c",
+            "lpc.c",
+            "lsp.c",
+            "mapping0.c",
+            "mdct.c",
+            "psy.c",
+            "registry.c",
+            "res0.c",
+            "sharedbook.c",
+            "smallft.c",
+            "synthesis.c",
+            "window.c",
+            "vorbisfile.c",
+            "vorbisenc.c",
         },
+        .root = b.path("libvorbis"),
     });
-    lib.linkLibC();
-    lib.installHeadersDirectory(b.path("include/vorbis"), "vorbis", .{});
-    b.installArtifact(lib);
+    libvorbis.installHeadersDirectory(b.path("include/vorbis"), "vorbis", .{});
+    b.installArtifact(libvorbis);
 }
